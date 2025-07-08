283b17985db4408c6be472376abe714f51584f43
asthasahu2004 <aasthasahu390@gmail.com>
2025-07-08 15:56:11 +0530

    test: hardcode SENDOTP_API

--- Client/src/Service/apis.js
+++ Client/src/Service/apis.js
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL || "https://study-ease-backend.onrender.com/api/v1"
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
+  SENDOTP_API: "https://studyease-backend.onrender.com/api/v1/auth/sendotp",
   SIGNUP_API: BASE_URL + "/auth/signup",
   LOGIN_API: BASE_URL + "/auth/login",
   RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
